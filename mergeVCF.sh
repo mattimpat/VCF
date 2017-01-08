5c0914082d5ba987768278376a13ffc4387c600c
Matthew Patrick <mattimpat@gmail.com>
2017-01-07 22:10:22 -0500

    Update mergeVCF.sh

--- mergeVCF.sh
+++ mergeVCF.sh
@@ -6,4 +6,4 @@ VCF1=$1; VCF2=$2; OUTPUT=$3
 getHeader(){ cat $1 | awk '!/^#/{exit}1' | head -n -1; }
 stripHeader(){ cat $1 | grep -vP '^#(?!CHROM)'; }
 
-{ getHeader ${VCF1}; paste <(stripHeader ${VCF1}) <(stripHeader ${VCF2}); } > ${OUTPUT}
+{ getHeader ${VCF1}; paste <(stripHeader ${VCF1}) <(stripHeader ${VCF2} | cut -f10-); } > ${OUTPUT}
